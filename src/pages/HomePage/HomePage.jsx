6b6e76a64a658c16f254099609925fb38a946777
nachosanson6 <nachosanson6@gmail.com>
2024-02-28 11:08:06 +0100

    navigation done, starting homepage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import "./HomePage.css"
+
+const HomePage = () => {
+    return (
+        <div className="homePage">
+            <div className="presentation">
+                <h1>Hola, soy Nacho</h1>
+                <h2>Desarrollador web Full-Stack y DevOps</h2>
+            </div>
+            <img src="/assets/images/Profile_Photo.png " alt="" />
+        </div>
+    )
+}
+
+export default HomePage
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import ProjectsPage from './../pages/ProjectsPage/ProjectsPage'
 import AboutPage from '../pages/AboutPage/AboutPage';
+import HomePage from '../pages/HomePage/HomePage';
 
 
 const AppRoutes = () => {
 
     return (
         <Routes>
-            {/* <Route path={'/'} element={<HomePage />} /> */}
+            <Route path={'/'} element={<HomePage />} />
             <Route path={'/projects'} element={<ProjectsPage />} />
             <Route path={'/about'} element={<AboutPage />} />
         </Routes>
