b9e0548793a3cbe5ec62fd5af855ba5d173c7483
nachosanson6 <nachosanson6@gmail.com>
2024-02-28 11:51:02 +0100

    homepage in progess

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,8 +6,12 @@ const HomePage = () => {
             <div className="presentation">
                 <h1>Hola, soy Nacho</h1>
                 <h2>Desarrollador web Full-Stack y DevOps</h2>
+                {/* Botón para dispositivos de escritorio y tablet */}
+                <button className="desktop-tablet-button mt-3">Download CV</button>
             </div>
-            <img src="/assets/images/Profile_Photo.png " alt="" />
+            <img src="/assets/images/Profile_Photo.png" alt="" />
+            {/* Botón para dispositivos móviles */}
+            <button className="mobile-button mt-3">Download CV</button>
         </div>
     )
 }
