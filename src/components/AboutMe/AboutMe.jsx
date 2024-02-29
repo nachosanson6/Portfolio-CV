3e83c9903bb9d635436e544c1b9650f00a38ce0f
nachosanson6 <nachosanson6@gmail.com>
2024-02-29 17:36:10 +0100

    about me in progress

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -5,7 +5,12 @@ const AboutMe = () => {
         <>
             <div className="aboutMe">
                 <h2 className="presentation" >Sobre Mi</h2>
-                <p className="text " >Soy un desarrollador web full-stack con un título del bootcamp de desarrollo web de Ironhack España. Me caracterizo por mi pasión, la dedicación que pongo en todo lo que me propongo, la consistencia, la organización y el deseo de aprender y mejorar cada día. Mi objetivo es ser parte de un equipo con la misma pasión que promueva buenas prácticas de desarrollo, donde pueda contribuir y aprender de ellos cada día. Durante mi tiempo en la industria, he tenido la oportunidad de trabajar en proyectos individuales y en grupo, lo que me ha permitido adquirir experiencia tanto en el desarrollo front-end como en el back-end. También he desarrollado habilidades de trabajo en equipo y algunas habilidades de liderazgo. Mis habilidades de comunicación y enseñanza también destacan debido a mi anterior puesto como maestro de educación primaria.</p>
+                <p className="text " >Soy un desarrollador web full-stack  y DevOps titulado por Ironhack España.
+                    Me caracterizo por mi pasión, la dedicación que pongo en todo lo que me propongo, la consistencia, la organización y el deseo de aprender y mejorar cada día.
+                    Mi objetivo es ser parte de un equipo con la misma pasión que promueva buenas prácticas de desarrollo, donde pueda contribuir y aprender de ellos cada día.
+                    Durante mi tiempo en la industria, he tenido la oportunidad de trabajar en proyectos individuales y en grupo, lo que me ha permitido adquirir experiencia tanto en el desarrollo front-end como en el back-end.
+                    También he desarrollado habilidades de trabajo en equipo y algunas habilidades de liderazgo.
+                    Mis habilidades de comunicación y enseñanza también destacan debido a mi anterior puesto como maestro de educación primaria.</p>
             </div>
 
         </>
