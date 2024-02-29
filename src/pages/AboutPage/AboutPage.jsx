01917a69fec4105e18ac13cdd982ddedb2969008
nachosanson6 <nachosanson6@gmail.com>
2024-02-29 14:01:44 +0100

    about me in progess

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,8 +1,10 @@
+import AboutMe from "../../components/AboutMe/AboutMe"
 import Skills from "../../components/Skills/Skills"
 import "./AboutPage.css"
 const AboutPage = () => {
     return (
         <div className="aboutPage">
+            <AboutMe />
             <Skills />
         </div>
     )
