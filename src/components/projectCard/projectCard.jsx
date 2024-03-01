6cf0131a99ab507687fb22b9ecf951413d5ed5ee
nachosanson6 <nachosanson6@gmail.com>
2024-03-01 09:06:05 +0100

    projects in progress

--- src/components/projectCard/projectCard.jsx
+++ src/components/projectCard/projectCard.jsx
@@ -2,15 +2,32 @@ import "./projectCard.css"
 
 
 const ProjectCard = ({ project }) => {
-    console.log(project)
     return (
         <div className="projectCard">
             <h2>{project.title}</h2>
             <p>{project.description}</p>
-            {project.skills.map(skill => (
-                <img src={`/assets/images/logo${skill}.png`} alt="" />
-            ))}
-        </div>
+            <div className="frame">
+                <div className="skillsList">
+                    {project.skills.map(skill => (
+                        <div className="skillsElements" key={`${project.title}_${skill}`}>
+                            <img src={`/assets/images/logo${skill}.png`} alt="" />
+                        </div>
+                    ))}
+                </div>
+                <div className="buttons">
+                    <a href={project.github_link} target="_blank" rel="noopener noreferrer">
+                        <button>
+                            GitHub
+                        </button>
+                    </a>
+                    <a href={project.web_link} target="_blank" rel="noopener noreferrer">
+                        <button>
+                            Web
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </div >
     )
 }
 
