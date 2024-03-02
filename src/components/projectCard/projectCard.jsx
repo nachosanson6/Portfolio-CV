67a3c999ef5d065b46b8f8b224e1bd86e99fcec5
nachosanson6 <nachosanson6@gmail.com>
2024-03-02 09:09:24 +0100

    arrow transition done

--- src/components/projectCard/projectCard.jsx
+++ src/components/projectCard/projectCard.jsx
@@ -2,25 +2,20 @@ import { useRef, useState, useEffect } from "react";
 import "./projectCard.css";
 
 const ProjectCard = ({ project }) => {
-    const [showButton, setShowButton] = useState(false); // Nuevo estado para controlar la visibilidad del botón
+    const [showButton, setShowButton] = useState(false);
     const [showDescription, setShowDescription] = useState(false);
     const descriptionRef = useRef(null);
 
     useEffect(() => {
-        // Verificar si hay overflow en la descripción
         if (descriptionRef.current && descriptionRef.current.scrollHeight > descriptionRef.current.clientHeight) {
-            setShowButton(true); // Mostrar el botón si hay overflow
+            setShowButton(true);
         } else {
-            setShowButton(false); // Ocultar el botón si no hay overflow
+            setShowButton(false);
         }
-    }, [project]); // Volvemos a verificar cada vez que cambia el proyecto
-
-    const showProjectDescription = () => {
-        setShowDescription(prevState => !prevState);
-    }
+    }, [project]);
 
     const rotateArrow = () => {
-        setShowDescription(prevState => !prevState); // Alternar el estado de mostrar descripción
+        setShowDescription(prevState => !prevState);
     };
 
     return (
@@ -34,6 +29,7 @@ const ProjectCard = ({ project }) => {
                     <button className={`arrowButton ${showDescription ? 'rotated' : ''}`} onClick={rotateArrow}>
                         <img src="/assets/images/arrow.svg" alt="Ver más" />
                     </button>
+
                 </div>
             )}
             <div className="frame">
