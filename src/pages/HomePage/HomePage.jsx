b2d6b46198f530e6c5710727cee91ec05262b0a1
nachosanson6 <nachosanson6@gmail.com>
2024-03-05 11:14:34 +0100

    contactModal done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
     const [isShowContact, setIsShowContact] = useState(false)
     const { scrollY } = useScroll()
     useMotionValueEvent(scrollY, "change", (latest) => {
-        if (latest >= 200) {
+        if (latest >= 250) {
             setIsShowContact(true)
         }
         else {
