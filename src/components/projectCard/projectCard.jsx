3620811f6611e6fda6eb1cb48dda5f84451823f0
nachosanson6 <nachosanson6@gmail.com>
2024-03-06 10:06:38 +0100

    language finished

--- src/components/projectCard/projectCard.jsx
+++ src/components/projectCard/projectCard.jsx
@@ -3,8 +3,12 @@ import "./projectCard.css";
 import Carousel from "../Carousel/Carousel";
 import { MdOutlineWebAsset } from "react-icons/md";
 import { FaSquareGithub } from "react-icons/fa6";
+import { useContext } from 'react'
+import { LanguageContext } from '../../contexts/language.context'
 
 const ProjectCard = ({ project, index, visible }) => {
+
+    const { language } = useContext(LanguageContext)
     const [showButton, setShowButton] = useState(false);
     const [showDescription, setShowDescription] = useState(false);
     const descriptionRef = useRef(null);
@@ -26,7 +30,7 @@ const ProjectCard = ({ project, index, visible }) => {
         <div className={`projectCard ${visible ? 'visible' : ''} slide-in-${index % 2 === 0 ? 'right' : 'left'}`} ref={cardRef}>
             <h2>{project.title}</h2>
             <div className={`projectDescription ${showDescription ? 'show' : ''}`} ref={descriptionRef}>
-                <p>{project.description}</p>
+                <p>{language === "spanish" ? project.description : project.description_en}</p>
             </div>
             {showButton && (
                 <div className="arrowButtonFrame">
